8dc6aa7e4ec0eec220596ab9d227baebd00d6625
Martin Lacayo-Emery <mlacayoemery@protonmail.com>
2024-10-18 07:02:14 -0400

    Updating to InVEST 3.14.1

---
+++ build.sh
@@ -0,0 +1,2 @@
+tagVersion=1.1.3_3.14.1
+docker build -t local/invest:$tagVersion .
--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 # can be any git commit-ish (tags, commits, branches)
-investCodeVersion=3.8.9
+investCodeVersion=3.14.1
 
 # check http://releases.naturalcapitalproject.org/?prefix=invest/3.8.9/data/ for
 # options.
 # NOTE: this data is only an example used for the tester UI, it is NOT used
 #       for running in production when the user supplies data.
-investDataUrl='https://www.googleapis.com/download/storage/v1/b/releases.naturalcapitalproject.org/o/invest%2F3.8.9%2Fdata%2Fpollination.zip?generation=1600197914247447&alt=media'
+investDataUrl='https://storage.googleapis.com/releases.naturalcapitalproject.org/invest/3.14.2/data/pollination.zip'
 
 # stop tzdata from prompting for a TZ, thanks https://stackoverflow.com/questions/44331836/apt-get-install-tzdata-noninteractive#comment91752692_44333806
 export DEBIAN_FRONTEND=noninteractive
@@ -16,9 +16,9 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get dist-upgrade --assume-yes
 apt-get install --assume-yes --no-install-recommends \
-  libgdal20 \
+  libgdal32 \
   libgdal-dev \
-  libspatialindex-c5
+  libspatialindex-c6
 
 
 pushd /data
@@ -29,6 +29,7 @@ rm $poll_zip
 mv pollination pollination-sample
 popd
 
+pip3 install --upgrade pip
 pip3 install numpy Cython ptvsd
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
